a8227c0c2af9c20dae266e24b83ca1aa8f330055
FilaCo <dvchizha@edu.hse.ru>
2019-01-04 14:40:12 +0300

    Создан класс BinList<T> для реализации тела змейки. Добавлен класс Map для реализации карты

---
+++ SnakeMain/SnakeMain/Bin_List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeMain
+{
+    class Bin_List<T>
+    {
+        private Bin_List<T> next, pref;
+        public T Data { get; set; }
+        public Bin_List(T elem)
+        {
+            next = pref = null;
+            Data = elem;
+        }
+        public void AddToEnd(T elem)
+        {
+            if (next != null)
+                next.AddToEnd(elem);
+            Bin_List<T> nElem = new Bin_List<T>(elem);
+            next = nElem;
+            nElem.pref = next;
+        }
+    }
+}
