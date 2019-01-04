29afb0fa58a2faf738d2ec322e53b7330c7f37bb
FilaCo <dvchizha@edu.hse.ru>
2019-01-04 16:43:02 +0300

    Создан класс Змейки и вспомогательный класс Point

--- SnakeMain/SnakeMain/Map.cs
+++ SnakeMain/SnakeMain/Map.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace SnakeMain
 {
-    enum TPoint { EMPTY, FRUIT, SNAKE }
+    enum MapPoint { EMPTY, FRUIT, SNAKE }
     class Map 
     {
         private const int MAX_WIDTH = 20, MAX_HEIGHT = 20;
-        private TPoint[,] matrix;
+        private MapPoint[,] matrix;
         public int Width { get { return matrix.GetLength(1); } }
         public int Height { get { return matrix.GetLength(0); } }
-        public TPoint this[int x, int y]
+        public MapPoint this[int x, int y]
         {
             get { return matrix[y, x]; }
             set { matrix[y, x] = value; }
         }
-        public Map(TPoint [,] _map)
+        public Map(MapPoint [,] _map)
         {
             if (_map.GetLength(0) > MAX_HEIGHT || _map.GetLength(1) > MAX_WIDTH)
                 throw new Exception("Размер карты больше максимального");
---
+++ SnakeMain/SnakeMain/Snake.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeMain
+{
+    class TPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public TPoint(int _x, int _y)
+        {
+            X = _x;
+            Y = _y;
+        }
+    }
+    public class Snake
+    {
+        public enum TDirection { UP, DOWN, LEFT, RIGHT};
+        private Bin_List<TPoint> body;
+        public TDirection Direct { get; set; }
+    }
+}
